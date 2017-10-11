154de8c07ab97b05e9acf01e1af92b61819bbf10
Avantgardian <andrew.avantgardian@gmail.com>
2017-10-11 17:05:51 +0300

    strings and messages

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Welcome to Bulls and Cows" << endl;
+	// intro to game
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls and Cows\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+
+	//get a guess from user
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	//repeat the guess back
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
 	return 0;
 }
